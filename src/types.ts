6cac85bd98049396a7c59b0ebe4a137c182d691c
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-09-17 20:08:08 +0200

    Add Car interface to types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Car {
+  id: string;
+  make: string;
+  model: string;
+  year: number;
+  price: number;
+  mileage: string;
+  image: string;
+  whatsapp: string;
+  city: string;
+  description: string;
+}
