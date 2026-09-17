72e16312ae968e57d5912391a8a8e30ec81c177f
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-09-17 18:43:13 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+// كود الواجهة الرئيسية والمنطق البرمجي للمنصة
+// يمكنك العثور على الكود الكامل في ملف App.tsx بمجلد src
+import React, { useState, useEffect } from 'react';
+import { Car } from './types';
+import { carService } from './lib/carService';
+// ... بقية الاستيرادات والمكونات
