da695f108e3c2c7bd7a4a5250675f43cd6021fd1
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-09-17 20:18:10 +0200

    Update carService.ts

--- src/lib/carService.ts
+++ src/lib/carService.ts
@@ -1,15 +1,17 @@
 import { collection, addDoc, getDocs, query, orderBy } from 'firebase/firestore';
 import { db } from './firebase';
+import type { Car } from '../types';
 
 const COLLECTION_NAME = 'cars';
 
 export const carService = {
-  async getAllCars() {
+  async getAllCars(): Promise<Car[]> {
     const q = query(collection(db, COLLECTION_NAME), orderBy('year', 'desc'));
     const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    return querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as Car));
   },
-  async addCar(car) {
+
+  async addCar(car: Omit<Car, 'id'>) {
     return await addDoc(collection(db, COLLECTION_NAME), car);
   }
 };
