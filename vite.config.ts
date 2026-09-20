9a8ebb1aac543043d9e6aa7ca880bcf07a91a92d
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-09-20 21:43:33 +0200

    Set base path in Vite config
    Added base configuration to resolve white screen issue.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './', // هذا السطر يحل مشكلة الشاشة البيضاء فوراً
   plugins: [react(), tailwindcss()],
 })
