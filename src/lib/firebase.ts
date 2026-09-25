a4007d79e796ddc009f6a61c0845bd6f56b21b20
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-09-25 19:58:06 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,30 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   projectId: "hotel-project-485811",
   appId: "1:693691274781:web:cc15f62ad313d881bad8f4",
   apiKey: "AIzaSyAZqYQwWRnQNkWwS6qPUBdkrexNhnURjgk",
   authDomain: "hotel-project-485811.firebaseapp.com",
+  firestoreDatabaseId: "ai-studio-carslibya-f8cf7d50-bd16-44a3-8284-47deae4aef16",
   storageBucket: "hotel-project-485811.firebasestorage.app",
-  messagingSenderId: "693691274781"
+  messagingSenderId: "693691274781",
+  measurementId: "",
+  oAuthClientId: "693691274781-v73g9lt0roekknvr25gvvcdn0vm1o2an.apps.googleusercontent.com"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, "ai-studio-carslibya-f8cf7d50-bd16-44a3-8284-47deae4aef16");
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const auth = getAuth(app);
+
+async function testConnection() {
+  try {
+    await getDocFromServer(doc(db, 'test', 'connection'));
+  } catch (error) {
+    if (error instanceof Error && error.message.includes('the client is offline')) {
+      console.error('يرجى التحقق من تكوين Firebase والاتصال بالإنترنت.');
+    }
+  }
+}
+testConnection();
